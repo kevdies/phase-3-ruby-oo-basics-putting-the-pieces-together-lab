c92c4942c24169831ff60e974a3e09e3d16e11d1
Kevin Diesenberg <kdiesenb@gmail.com>
2022-12-13 12:23:33 -0500

    init and method together

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+require "pry"
 
+class Book
+    attr_reader :title
+    # this will be book.title  we don't need to change this
 
+    # the book.title will have a author, pages, genre
+        # we need accessor to read and write those attributes
+    attr_accessor :author, :page_count, :genre
+
+    def initialize(title)
+        @title = title
+        # initialize the title and the other attributes can be read and written over afterwards
+    end
+
+    # need to have method turn_page
+    # turn_page will OUPUT a string "Flipping the page...wow, you read fast!"
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
